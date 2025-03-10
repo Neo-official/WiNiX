b41918e5832727d9dc7e9476b8d212bd8e1013a1
neo_o <25305924+Neo-official@users.noreply.github.com>
2025-03-10 18:50:31 +0330

    Took 2 minutes

--- web-app/next.config.ts
+++ web-app/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const isMobile = process.env.NEXT_PUBLIC_IS_MOBILE === 'true';
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? '';
 const nextConfig: NextConfig = {
 	...(isMobile ? {output: 'export',distDir: '../www'} : {}),
+	basePath: basePath,
+	assetPrefix: `${basePath}/`,
 	reactStrictMode: true,
 	images: {
 		unoptimized: true, // Required for static export
